2df1299ea14146b0b44b84d005bdb49c9b5333d8
Thái Hồ Quốc <thaiminh2022@gmail.com>
2026-05-10 21:02:43 +0700

    add shits

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -73,7 +73,12 @@ return {
     {
         "folke/noice.nvim",
         event = "VeryLazy",
-
+        opts = {
+            cmdline = {
+                enabled = true,
+                view = "cmdline_popup",
+            },
+        },
         dependencies = {
             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
